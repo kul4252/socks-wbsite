bc8b22480c765c05444f290e1fb3744846a183c1
incora-dev <dev@incorainc.com>
2018-10-04 17:18:54 +0300

    add models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length= 256, unique= True)
+    description = models.TextField()
+    options = models.ManytoManyField("Option")
+    category = models.ManytoManyField("Category")
+    unit_price = models.FloatField()
+    discount_price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def price(self):
+        return self.discount_price if self.discount_price else self.unit_price
+
+
+class Category(models.Model):
+    name = models.CharField(max_length= 256)
+    slug = models.CharField(max_length= 256)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class ProductOrder(models.Model):
+    product = models.ForeignKey("Product", on_delete= models.CASCADE)
+    count = models.IntegerField(default= 1)
+
+class Order(model.Model):
+    address = models.CharField(max_length= 256)
+    phone_number = models.CharField(max_length= 32)
+    email = models.CharField(max_length= 32)
+    comment = models.TextField()
+    name = models.CharField(max_length= 256)
+    sname = models.CharField(max_length= 256)
+
+    products = models.ManytoManyField("ProductOrder")
+
+    created_at = models.DateTimeField(auto_now_add= True)
+
+    def __str__(self):
+        return "Order %i" % self.pk
+
+    @property
+    def total(self):
+        return sum([po.product.price * po.count for po in products])
--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7as(7qb=1(xnctr=-#xtdhr@wfx2cbp5$5eiyhsqmb!c_%jt)z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
